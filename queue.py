34232a430af6865f849d8c8d4749d8b91c330b1a
Sidra Ghaffar <sidraghaffar04@gmail.com>
2026-02-10 07:45:10 +0500

    Enhance Queue class with interactive menu
    Refactor queue operations and add user interaction loop.

--- queue.py
+++ queue.py
@@ -36,21 +36,31 @@ n=int(input("How many you want to insert initaially?"))
 for i in range(n):
     val=input(f"Enter the value {i+1} =")
     obj.enqueue(val)
-
-print("choose your operation")
-print("1.Add the element in queue")
-print("2.delete the element in queue")
-print("3.show the first element")
-op=input("Enter the operation(1,2,3):")
-if op=='1':
-    val2=input("Enter a new value =")
+obj.display()   
+while True:
+ print("choose your operation")
+ print("1.Add the element in queue")
+ print("2.delete the element in queue")
+ print("3.show the front element")
+ print("4.Exit from the program")
+ op=input("Enter the operation(1,2,3):")
+ if op=='1':
+    val2=input("Enter a value =")
     obj.enqueue(val2)
+    obj.display()
     
-elif op=='2':
+ elif op=='2':
     obj.dequeue()
+    obj.display()
   
-elif op=='3':
+ elif op=='3':
     obj.peep()
+    obj.display()
 
-obj.display()
+ elif op=='4':
+     print("the final queue:",obj.queue)
+     break
+ else:
+     print("Invalid choice ,please enter a valid choice")   
+
    
