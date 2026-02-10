091772c45ddbd8e953533eaf1da7ae3e31f7046e
Sidra Ghaffar <sidraghaffar04@gmail.com>
2026-02-10 07:29:49 +0500

    Add files via upload

---
+++ queue.py
@@ -0,0 +1,56 @@
+class Queue:
+    def __init__(self):
+        self.queue=[]
+
+
+    def isEmpty(self):
+        return len(self.queue)==0
+    
+
+    def enqueue(self,data):
+        self.queue.append(data)
+
+
+    def dequeue(self):
+        if self.isEmpty():
+            print("Queue is empty")
+        else:
+            remove=self.queue.pop(0)
+            print(f"The {remove} is pop out")
+        
+
+    def peep(self):
+        if self.isEmpty():
+            print("Queue is empty")
+        else:
+            print("The front element is",self.queue[0])
+        
+
+    def display(self):
+        print("The current Queue is:",self.queue)
+        
+
+
+obj=Queue()
+n=int(input("How many you want to insert initaially?"))
+for i in range(n):
+    val=input(f"Enter the value {i+1} =")
+    obj.enqueue(val)
+
+print("choose your operation")
+print("1.Add the element in queue")
+print("2.delete the element in queue")
+print("3.show the first element")
+op=input("Enter the operation(1,2,3):")
+if op=='1':
+    val2=input("Enter a new value =")
+    obj.enqueue(val2)
+    
+elif op=='2':
+    obj.dequeue()
+  
+elif op=='3':
+    obj.peep()
+
+obj.display()
+   
